82106c72edca08d6e8918dd71b1dea11db7e301a
ifdhalrumalutur <ifdhal060902@gmail.com>
2025-09-27 12:36:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <H1>Hello World</H1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ad saepe nam, natus provident, sunt modi voluptatum voluptates quisquam ratione commodi possimus dignissimos, aliquid rerum dolores animi quos porro id.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
