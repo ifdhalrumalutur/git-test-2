742791ea56ece6a49903cae2a98e2343bfb6b94e
ifdhalrumalutur <ifdhal060902@gmail.com>
2025-09-27 12:31:22 +0700

    menambahakan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <H1>Hello World</H1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ad saepe nam, natus provident, sunt modi voluptatum voluptates quisquam ratione commodi possimus dignissimos, aliquid rerum dolores animi quos porro id.</p>
 </body>
 </html>
